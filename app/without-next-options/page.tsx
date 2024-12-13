bdbed1c80d88ae30cc1c4db1790bba6b19e3f5b4
Sunghyun Cho <sunghyun.cho@lunit.io>
2024-12-13 11:03:33 +0900

    Add dynamic export in page.tsx

--- app/without-next-options/page.tsx
+++ app/without-next-options/page.tsx
@@ -1,5 +1,7 @@
 import { firestore } from "../count";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const res = await firestore({ withNextOptions: false });
   return <div>{JSON.stringify(res)}</div>;
